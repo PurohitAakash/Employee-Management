77e14f6a709f2c99c6e8718b430ac4d6c6ee53d1
PurohitAakash <purohitaakash94@gmail.com>
2024-11-11 23:51:23 +0530

    first commit in this repo

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <div>
+        <footer className="footer">
+            <span>All Rights Reserved By Aakash</span>
+        </footer>
+    </div>
+    )
+}
+export default Footer
